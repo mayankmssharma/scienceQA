1726bdb3a9975d43a9707da681402e2503a342a0
naigamshah <naigamshah@Naigams-MacBook-Air.local>
2023-03-22 16:32:02 -0700

    Added contrastive loss implementation in ContrastiveModel

--- train_multiqa.py
+++ train_multiqa.py
@@ -17,18 +17,18 @@ import torch.nn as nn
 from torch.optim import AdamW
 from torch.utils.data import Dataset, DataLoader
 
-from models import Model, ContrastiveModel
+from models import MultiModel
 from transformers import get_linear_schedule_with_warmup
 from transformers.trainer_pt_utils import get_parameter_names
 from transformers.optimization import Adafactor, get_scheduler
 
 from collections import defaultdict
-from datasets import load_from_disk, concatenate_datasets
+from datasets import load_from_disk
 from sklearn.metrics import accuracy_score, f1_score
 
-class ContrastiveScienceQADataset(Dataset):
+class MultiScienceQADataset(Dataset):
     def __init__(self, data, shuffle):
-        data = pd.DataFrame(data)[["question","choices","answer","hint"]]
+        data = pd.DataFrame(data)[["question","choices","answer","hint","img_embed"]]
         if shuffle:
             data = data.sample(frac=1)
 
@@ -36,16 +36,16 @@ class ContrastiveScienceQADataset(Dataset):
         self.choices = list(data["choices"].values)
         self.hint = list(data["hint"].values)
         self.answer = list(data["answer"].values)
+        self.img_embed = list(data["img_embed"].values)
         
-        del data
         print(len(self.question))
         
     def __len__(self):
         return len(self.question)
 
     def __getitem__(self, index):
-        s1, s2, s3, s4 = self.question[index], self.choices[index], self.hint[index], self.answer[index]
-        return s1, s2, s3, s4
+        s1, s2, s3, s4, s5 = self.question[index], self.choices[index], self.hint[index], self.answer[index], self.img_embed[index]
+        return s1, s2, s3, s4, s5
     
     def collate_fn(self, data):
         dat = pd.DataFrame(data)
@@ -54,17 +54,15 @@ class ContrastiveScienceQADataset(Dataset):
     
 def configure_dataloaders(train_batch_size=16, eval_batch_size=16, shuffle=False):
     "Prepare dataloaders"
-    data = load_from_disk("data/scienceqa/scienceQA")
-    
-    data["train"] = concatenate_datasets([data["train"], data["validation"]])
+    data = load_from_disk("data/scienceqa/scienceQA_new")
 
-    train_dataset = ContrastiveScienceQADataset(data["train"], True)
+    train_dataset = MultiScienceQADataset(data["train"], True)
     train_loader = DataLoader(train_dataset, shuffle=shuffle, batch_size=train_batch_size, collate_fn=train_dataset.collate_fn)
 
-    val_dataset = ContrastiveScienceQADataset(data["validation"], False)
+    val_dataset = MultiScienceQADataset(data["validation"], False)
     val_loader = DataLoader(val_dataset, shuffle=False, batch_size=eval_batch_size, collate_fn=val_dataset.collate_fn)
     
-    test_dataset = ContrastiveScienceQADataset(data["test"], False)
+    test_dataset = MultiScienceQADataset(data["test"], False)
     test_loader = DataLoader(test_dataset, shuffle=False, batch_size=eval_batch_size, collate_fn=val_dataset.collate_fn)
 
     return train_loader, val_loader, test_loader
@@ -102,31 +100,32 @@ def configure_scheduler(optimizer, num_training_steps, args):
     return lr_scheduler
 
 def expand_batch(batch, input_format):
-    content, options, labels = [], [], []
-    for i, (q, choices, h, a) in enumerate(zip(*batch)):
+    content, img_embeds, labels = [], [], []
+    for i, (q, choices, h, a, ie) in enumerate(zip(*batch)):
         #Plain text
         if input_format == "0":
             for c in choices:
-                content.append("{}".format(q))
-                options.append("{}".format(q,c))
+                content.append("{} {}".format(q,c))
 
         #Add prompts for question and option
         elif input_format == "1":
             for c in choices:
-                content.append("Question: {}".format(q))
-                options.append("Option: {}".format(c))
+                content.append("Question: {}, Option: {}". format(q, c))
 
         #Add prompts for question, hint and option
         elif input_format=="2":
             for c in choices:
-                content.append("Question: {}, Hint: {}". format(q, h))
-                options.append("Option: {}".format(c))
+                content.append("Question: {}, Hint: {}, Option: {}". format(q, h, c))
 
+        #Append img_embed for every choice        
+        for c in choices:
+            img_embeds.append(ie)
+            
         y = [0]*len(choices)
         y[a] = 1
-        labels+=y
+        labels += y
         
-    return content, options, labels
+    return content, img_embeds, labels
 
 def regroup_batch(sizes, l, p):
     it_l, it_p = iter(l), iter(p)
@@ -134,12 +133,6 @@ def regroup_batch(sizes, l, p):
     preds = [np.argmax(list(itertools.islice(it_p, size))) for size in sizes]
         
     return labels, preds
-
-def contrastive_loss(scores, labels):
-    correct_score, incorrect_scores = scores[0], scores[1:]
-    margin = 0.1
-    loss = torch.mean(torch.max(incorrect_scores - correct_score + margin, torch.zeros_like(incorrect_scores)))
-    return loss
 
 def train_or_eval_model(model, dataloader, optimizer=None, input_format="0", split="Train"):
     losses, labels, preds, preds_cls, labels_cls,  = [], [], [], [], []
@@ -154,18 +147,17 @@ def train_or_eval_model(model, dataloader, optimizer=None, input_format="0", spl
             
         #Store length of choices to re-group them later
         sizes = [len(x) for x in batch[1]]
-        #print(sizes)
         
-        content, choices, lbls = expand_batch(batch, input_format)
-        loss, p = model(content, choices, lbls)
-                
-        # Regroup
-        ls, ps = regroup_batch(sizes, lbls, p)
+        content, img_embeds, l_cls = expand_batch(batch, input_format)
+        loss, p, p_cls = model((content, img_embeds, l_cls))
+        
+        #Regroup
+        ls, ps = regroup_batch(sizes, l_cls, p)
         
         labels.append(ls)
         preds.append(ps)
-        # preds_cls.append(p_cls)
-        # labels_cls.append(l_cls)
+        preds_cls.append(p_cls)
+        labels_cls.append(l_cls)
         
         if split=="Train":
             # wandb.log({"Train Step Loss": loss})
@@ -180,26 +172,40 @@ def train_or_eval_model(model, dataloader, optimizer=None, input_format="0", spl
     
     if split=="Train":
         wandb.log({"Train Loss": avg_loss})
+        all_preds_cls = [item for sublist in preds_cls for item in sublist]
+        all_labels_cls = [item for sublist in labels_cls for item in sublist]
+        acc = round(accuracy_score(all_labels_cls, all_preds_cls), 4)
+        f1 = round(f1_score(all_labels_cls, all_preds_cls, average="macro"), 4)
+        wandb.log({"Train CLS Accuracy": acc})
         
         instance_preds = [item for sublist in preds for item in sublist]
         instance_labels = [item for sublist in labels for item in sublist]
         instance_acc = round(accuracy_score(instance_labels, instance_preds), 4)
         wandb.log({"Train Instance Accuracy": instance_acc})
         
-        return avg_loss, instance_acc
+        return avg_loss, acc, instance_acc, f1
     
     elif split=="Val":
         wandb.log({"Val Loss": avg_loss})
+        all_preds_cls = [item for sublist in preds_cls for item in sublist]
+        all_labels_cls = [item for sublist in labels_cls for item in sublist]
+        acc = round(accuracy_score(all_labels_cls, all_preds_cls), 4)
+        f1 = round(f1_score(all_labels_cls, all_preds_cls, average="macro"), 4)
+        wandb.log({"Val CLS Accuracy": acc})
         
         instance_preds = [item for sublist in preds for item in sublist]
         instance_labels = [item for sublist in labels for item in sublist]
         instance_acc = round(accuracy_score(instance_labels, instance_preds), 4)
         wandb.log({"Val Instance Accuracy": instance_acc})
         
-        return avg_loss, instance_acc
+        return avg_loss, acc, instance_acc, f1
     
     elif "Test" in split:
-        wandb.log({"Test Loss": avg_loss})
+        all_preds_cls = [item for sublist in preds_cls for item in sublist]
+        all_labels_cls = [item for sublist in labels_cls for item in sublist]
+        acc = round(accuracy_score(all_labels_cls, all_preds_cls), 4)
+        f1 = round(f1_score(all_labels_cls, all_preds_cls, average="macro"), 4)
+        wandb.log({"Test CLS Accuracy": acc})
         
         instance_preds = [item for sublist in preds for item in sublist]
         instance_labels = [item for sublist in labels for item in sublist]
@@ -209,7 +215,7 @@ def train_or_eval_model(model, dataloader, optimizer=None, input_format="0", spl
         instance_preds = [str(item) for item in instance_preds]
         print ("Test preds frequency:", dict(pd.Series(instance_preds).value_counts()))
 
-        return instance_preds, avg_loss, instance_acc
+        return instance_preds, avg_loss, acc, instance_acc, f1
     
     
 if __name__ == "__main__":
@@ -243,10 +249,9 @@ if __name__ == "__main__":
     num_choices = -1
     vars(args)["num_choices"] = num_choices
     
-    model = ContrastiveModel(
+    model = MultiModel(
         name=name,
         num_choices=num_choices,
-        out_size=100,
         device = device
     ).to(device)
     
@@ -287,25 +292,29 @@ if __name__ == "__main__":
     
     for e in range(epochs):    
         
-        train_loss, train_ins_acc = train_or_eval_model(model, train_loader, optimizer, input_format=input_format, split="Train")
-        val_loss, val_ins_acc = train_or_eval_model(model, val_loader, input_format=input_format, split="Val")
-        test_preds, test_loss, test_ins_acc = train_or_eval_model(model, test_loader, input_format=input_format, split="Test")
+        train_loss, train_acc, train_ins_acc, train_f1 = train_or_eval_model(model, train_loader, optimizer, input_format=input_format, split="Train")
+        val_loss, val_acc, val_ins_acc, val_f1 = train_or_eval_model(model, val_loader, input_format=input_format, split="Val")
+        test_preds, test_loss, test_acc, test_ins_acc, test_f1 = train_or_eval_model(model, test_loader, input_format=input_format, split="Test")
         
-        # with open(path + "-epoch-" + str(e+1) + ".txt", "w") as f:
-        #     f.write("\n".join(list(test_preds)))
+        with open(path + "-epoch-" + str(e+1) + ".txt", "w") as f:
+            f.write("\n".join(list(test_preds)))
         
         x = "Epoch {}: Loss: Train {}; Val {}; Test {}".format(e+1, train_loss, val_loss, test_loss)
+        y1 = "Classification Acc: Train {}; Val {}; Test {}".format(train_acc, val_acc, test_acc)
+        y2 = "Classification Macro F1: Train {}; Val {}; Test {}".format(train_f1, val_f1, test_f1)
         z = "Instance Acc: Train {}; Val {}; Test {}".format(train_ins_acc, val_ins_acc, test_ins_acc)
             
         print (x)
+        print (y1)
+        print (y2)
         print (z)
 
         lf = open(lf_name, "a")
-        lf.write(x + "\n" + z + "\n\n")
+        lf.write(x + "\n" + y1 + "\n" + y2 + "\n" + z + "\n\n")
         lf.close()
 
         f = open(fname, "a")
-        f.write(x + "\n" + z + "\n\n")
+        f.write(x + "\n" + y1 + "\n" + y2 + "\n" + z + "\n\n")
         f.close()
         
     lf = open(lf_name, "a")
