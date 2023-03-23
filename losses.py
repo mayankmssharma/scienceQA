8731d7e7671de55f34355fc6d42773561ff1509d
naigamshah <naigamshah@Naigams-MacBook-Air.local>
2023-03-22 17:40:23 -0700

    Added ContrastiveLoss

--- losses.py
+++ losses.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class HingeLoss(nn.Module):
     def __init__(self, margin):
@@ -10,3 +11,16 @@ class HingeLoss(nn.Module):
         distances = torch.sum((embeddings1 - embeddings2) ** 2, dim=1)
         loss = torch.mean((torch.ones(len(labels), device=distances.device) - labels) * torch.max(distances - self.margin, torch.tensor(0.0, device=distances.device)))
         return loss
+    
+    
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, x1, x2, y):
+        euclidean_distance = F.pairwise_distance(x1, x2)
+        loss_contrastive = torch.mean((y) * torch.pow(euclidean_distance, 2) +
+                                      (1 - y) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+        return loss_contrastive
